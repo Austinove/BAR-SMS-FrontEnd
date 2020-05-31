59eddaad1a7a11faf49a4df7f73452d879fb196d
Austinove <bryanovicaustenove@gmail.com>
2020-05-31 18:07:19 +0300

    counter actions

---
+++ src/actions/counterAction.js
@@ -0,0 +1,73 @@
+import { counterConstants } from "../constants/index";
+
+//Fetching
+export const couterItemsRequest = () => {
+    return {
+        type: counterConstants.fetch_counterItems,
+    };
+};
+export const couterItemsSuccess = items => {
+    return {
+        type: counterConstants.fetch_counterItems_success,
+        items
+    };
+};
+export const couterItemsFailure = () => {
+    return {
+        type: counterConstants.fetch_counterItems_fail,
+    };
+};
+//Adding
+export const addCounterItemRequest = itemData => {
+    return {
+        type: counterConstants.add_to_counter,
+        itemData
+    }
+}
+export const addCounterItemSuccess = itemData => {
+    return {
+        type: counterConstants.add_to_counter_success,
+        itemData
+    }
+}
+export const addCounterItemFail = () => {
+    return {
+        type: counterConstants.add_to_counter_fail
+    }
+}
+//Removing
+export const removeCounterItemRequest = itemData => {
+    return {
+        type: counterConstants.remove_from_counter,
+        itemData
+    }
+}
+export const removeCounterItemSuccess = itemData => {
+    return {
+        type: counterConstants.remove_from_counter_success,
+        itemData
+    }
+}
+export const removeCounterItemFail = () => {
+    return {
+        type: counterConstants.remove_from_counter_fail
+    }
+}
+//Saling
+export const saleRequest = itemData => {
+    return {
+        type: counterConstants.make_sale,
+        itemData
+    }
+}
+export const saleSuccess = itemData => {
+    return {
+        type: counterConstants.make_sale_success,
+        itemData
+    }
+}
+export const saleFail = () => {
+    return {
+        type: counterConstants.make_sale_fail
+    }
+}
